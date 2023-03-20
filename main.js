7f483c937b529ad24600e1c4c2510dd5719c0ae0
lukasolivares <lucasolivares.tw@gmail.com>
2023-03-20 17:24:00 -0300

    a

--- main.js
+++ main.js
@@ -1,54 +1,23 @@
-let nombre = prompt("ingrese nombre")
-alert("Bienvenido/a " + nombre)
+function solicitarNombre() {
+  let ingresarNombre  = prompt("Ingresar Nombre")
+  alert("Bienvenido/a " + ingresarNombre)
+}
+
+function solicitarPeso() {
+  let ingresarPeso = prompt("Ingresa tu Peso en Kilogramos(kg)")
+  alert("Tu peso es")
+}
 
 let repetir = true
 
+
 while (repetir) {
-  let verdura = prompt("Ingrese la verdura que desea comprar.")
-
-  switch (verdura) {
-    case "cebolla":
-      alert("el kilo de cebolla $400")
-      break;
-    case "papa":
-      alert("el kilo de papa $40")
-      break;
-
-    default:
-      alert("no tenemos la verdura ingresada")
-      break;
-  }
-
-  let respuesta = prompt("Desea comprar otra verdura? escriba Si o No")
-
-  if (respuesta === "no" || respuesta === "No") {
-    repetir = false
-  }
+  let SolicitarPeso  = prompt("Ingresa tu Peso en Kilogramos(kg)")
 }
 
-alert("Gracias por su compra " + nombre)
+solicitarNombre ()
 
-const verduras = [
-  {
-    nombre: "cebolla",
-    precio: 400,
-    oferta: false
-  },
-  {
-    nombre: "papa",
-    precio: 40,
-    oferta: true
-  },
-  {
-    nombre: "zanahoria",
-    precio: 50,
-    oferta: true
-  },
-]
 
-for (let i = 0; i < verduras.length; i++) {
-  let verdura = verduras[i]
-  if (verdura.oferta) {
-    alert("La verdura " + verdura.nombre + " tiene oferta")
-  }
-}
+
+
+
